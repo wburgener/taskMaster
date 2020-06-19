0dfb60699014ae51c59bbee9d1df9e893f687b02
sedonatooley <too16003@byui.edu>
2020-06-19 16:27:01 -0600

    adding base activities

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendToMakeSchedule(View view) {
+        Intent intent = new Intent(this, MakeSchedule.class);
+        startActivity(intent);
+    }
+
+    public void sendToViewSchedule(View view) {
+        Intent intent = new Intent(this, ViewSchedule.class);
+        startActivity(intent);
+    }
+
+    public void sendToCalendar(View view) {
+        Intent intent = new Intent(this, Calendar.class);
+        startActivity(intent);
+    }
+
+    public void sendToSchedules(View view) {
+        Intent intent = new Intent(this, Schedules.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/taskmaster/MakeSchedule.java
@@ -0,0 +1,14 @@
+package com.example.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MakeSchedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_schedule);
+    }
+}
