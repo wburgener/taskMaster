02da50415d4f31319e6afd6ebaffb4f000f82b4f
jose-aquinof <65300636+jose-aquinof@users.noreply.github.com>
2020-06-23 14:13:50 -0600

    I added the Calendar View.

--- app/src/main/java/com/example/taskmaster/Calendar.java
+++ app/src/main/java/com/example/taskmaster/Calendar.java
@@ -3,9 +3,11 @@ package com.example.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
 
 public class Calendar extends AppCompatActivity {
 
+    private CalendarView mCalendarView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
