caa19be06e2dfd03a7bee89bbf6ba2f0c3946c1c
sedonatooley <too16003@byui.edu>
2020-06-25 18:50:14 -0600

    first work on saving user input

---
+++ app/src/main/java/com/example/taskmaster/AppConfig.java
@@ -0,0 +1,7 @@
+package com.example.taskmaster;
+
+final class AppConfig {
+    static final String EXTRA_CRITICAL = "edu.byui.prove_05_stretch.EXTRA_BOOK";
+    static final String EXTRA_ACTIVITY = "edu.byui.prove_05_stretch.EXTRA_CHAPTER";
+    static final String FILE_PREF = "edu.byui.prove_05_stretch.FILE_PREF";
+}
--- app/src/main/java/com/example/taskmaster/MakeSchedule.java
+++ app/src/main/java/com/example/taskmaster/MakeSchedule.java
@@ -2,13 +2,37 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 
 public class MakeSchedule extends AppCompatActivity {
+
+    private String activity;
+    private String critical;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_schedule);
+
+        // Populate the fields based on the Intent Extra's
+        Intent intent = getIntent();
+        critical = intent.getStringExtra(AppConfig.EXTRA_CRITICAL);
+        activity = intent.getStringExtra(AppConfig.EXTRA_ACTIVITY);
+    }
+
+    public void saveSchedule(View view) {
+        // Get a shared preferences handle for the application
+        SharedPreferences sharedPref = this.getSharedPreferences(
+                AppConfig.FILE_PREF, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        // Set the configuration values and commit
+        editor.putString(AppConfig.EXTRA_CRITICAL, critical);
+        editor.putString(AppConfig.EXTRA_ACTIVITY, activity);
+        editor.commit();
     }
 }
