521fe9fe9df9234297d3b4ffc150342b39449736
Wesley <wesleyburgener@gmail.com>
2020-06-25 18:58:24 -0600

    Merging Files

--- app/src/main/java/com/example/taskmaster/MakeSchedule.java
+++ app/src/main/java/com/example/taskmaster/MakeSchedule.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import java.util.Calendar;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -14,6 +18,8 @@ import android.widget.TextView;
 
 public class MakeSchedule extends AppCompatActivity {
 
+    private String activity;
+    private String critical;
     private TextView mDisplayDate;
     private DatePickerDialog.OnDateSetListener mDateSetListener;
     private static final String TAG = "MakeSchedule";
@@ -23,6 +29,11 @@ public class MakeSchedule extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_schedule);
         mDisplayDate = (TextView) findViewById(R.id.Date);
+
+        // Populate the fields based on the Intent Extra's
+        Intent intent = getIntent();
+        critical = intent.getStringExtra(AppConfig.EXTRA_CRITICAL);
+        activity = intent.getStringExtra(AppConfig.EXTRA_ACTIVITY);
 
         mDisplayDate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,5 +62,16 @@ public class MakeSchedule extends AppCompatActivity {
                 mDisplayDate.setText(date);
             }
         };
+
+//        public void saveSchedule(View view) {
+//            // Get a shared preferences handle for the application
+//            SharedPreferences sharedPref = this.getSharedPreferences(
+//                    AppConfig.FILE_PREF, Context.MODE_PRIVATE);
+//            SharedPreferences.Editor editor = sharedPref.edit();
+//            // Set the configuration values and commit
+//            editor.putString(AppConfig.EXTRA_CRITICAL, critical);
+//            editor.putString(AppConfig.EXTRA_ACTIVITY, activity);
+//            editor.commit();
+//        }
     }
 }
