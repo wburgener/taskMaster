eae6b0804c62cbf5c0bbd9791abfa97883200e1d
EmmaGA17 <33585584+EmmaGA17@users.noreply.github.com>
2020-07-14 20:18:10 -0600

    Started working on timing and notification system

--- app/src/main/java/com/example/taskmaster/ViewSchedule.java
+++ app/src/main/java/com/example/taskmaster/ViewSchedule.java
@@ -1,8 +1,12 @@
 package com.example.taskmaster;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +16,8 @@ import android.widget.Toast;
 
 
 import java.io.File;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 
@@ -23,20 +29,27 @@ public class ViewSchedule extends AppCompatActivity {
     String task;
     Boolean complete;
     final Schedule schedule = new Schedule();
+    private int notificationId = 1;
+    ArrayList<LocalTime> timeList = new ArrayList<LocalTime>();
+    LocalTime rightnow;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_schedule);
+        display();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     void display()
     {
 
-        int startTime = schedule.startTime;
         int howMany = schedule.list.size();
         LinearLayout linearLayout = new LinearLayout(this);
         setContentView(linearLayout);
         linearLayout.setOrientation(LinearLayout.VERTICAL);
+        Time();
         for (i = 0; i < howMany; i++)
         {
             TextView textView = new TextView(this);
@@ -62,12 +75,54 @@ public class ViewSchedule extends AppCompatActivity {
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void Time()
+    {
+        LocalTime st = schedule.startTime;
+        LocalTime time = st;
+        int howMany = schedule.list.size();
+        for (i = 0; i < howMany; i++)
+        {
+            long offset = schedule.list.get(i).length;
+            time = st.plusHours(offset);
+            timeList.add(i, time);
+
+            if (rightnow.now() == time)
+            {
+
+            }
+        }
+
+    }
+
+
     public void Progress(View view)
     {
         ProgressBar p = new ProgressBar(schedule, this);
         Thread threadProgress = new Thread (p, "progress bar");
         threadProgress.start();
     }
+
+   /* public void Notify()
+    {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon()
+                .setContentTitle("Your Next Task")
+                .setContentText()
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManager notificationManger =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManger.notify(notificationId, builder.build());
+
+    }
+
+    */
+
+
+
+
+
 
 
 
